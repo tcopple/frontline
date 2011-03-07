89d5db7580ace25ed4b8049df32e326b152ab653
Tyler Copple <J.TylerCopple@gmail.com>
2011-03-06 19:15:58 -0500

    several changes that should have been pushed with the last batch to handle new file structure

--- index.php
+++ index.php
@@ -1,49 +1,41 @@
-<head>
-  <title>frontline city church</title>
-  <link rel="stylesheet" type="text/css" href="css/reset.css" />
-  <link rel="stylesheet" type="text/css" href="css/layout.css" />
-  <link rel="stylesheet" type="text/css" href="css/global.css" />
-  <link rel="stylesheet" type="text/css" href="css/frontline.css" />
-  <link rel="stylesheet" type="text/css" href="css/superfish.css" />
-  <script type="text/javascript" src="js/jquery-1.4.4.min.js"></script> 
-  <script type="text/javascript" src="js/superfish.js"></script>
-  <script type="text/javascript" src="js/frontline.js"></script>
-  <script type="text/javascript" src="js/vimeo_gallery.js"></script>
-</head>
-<body>
-  <div class="container_16">
-    <?php include("layouts/header.php")?>
-    <div class="grid_16">
-      <div id="content">
-        <div id="promos">sssshhhh.... dont' tell anyone we're testing our site with their content... :)</div>
-        <div class="seperator"></div>
-        <div id="videos">
-            <video width=720 height=402 src="../videos/video1.m4v" poster="http://cdn.marshillchurch.org/files/2010/11/21/20101121_west-seattle_poster_img.jpg" controls="">1</video>
-            <video width=720 height=402 src="../videos/video1.m4v" poster="http://cdn.marshillchurch.org/files/2010/11/28/20101128_best-of-how-human-was-jesus_poster_img.jpg" controls="" >2</video>
-            <video width=720 height=402 src="../videos/video1.m4v" poster="http://cdn.marshillchurch.org/files/2010/11/21/20101121_ministry-marriage-and-mistakes_poster_img.jpg" controls="">3</video>
-        </div>
-      </div>
-    </div>
+<?php 
+  include_once("utilities.php"); 
+  start(
+    "<script type=\"text/javascript\" src=\"js/movie_gallery.js\"></script>"
+  ); 
+?>
+  <div id="videos">
+    <?php
+      //Build video links from available files in videos directory.
+      foreach (glob("videos/*.m4v") as $video) {
+        $tag = "<video width=\"720px\" height=\"402px\" src=\"$video\" ";
+        $poster = str_replace("m4v", "jpg", $video);
+
+        if(file_exists($poster))
+          $tag .= "poster=\"$poster\" ";
+
+        $tag .= "controls></video>";
+        echo $tag; 
+      }
+    ?>
+  </div>
+</div>
     <div class="clear" style="height: 25px;"></div>
     <div class="grid_16">
       <div id="info">
         <div class="box">
-        <div class="feed">test</div>
+          <div class="feed">test</div>
         </div>
         <div style="width:50px; float: left;">&nbsp</div>
         <div class="box">
-        <div class="feed">test</div>
+          <div class="feed">test</div>
         </div>
         <div style="width:50px; float: left;">&nbsp</div>
         <div class="box">
-        <div class="feed">test</div>
+          <div class="feed">test</div>
         </div>
       </div>
     </div>
-    <div class="clear"></div>
-    <?php include("layouts/footer.php")?>
-  </div>
+  <?php include("layouts/footer.php") ?>;
+</div> <!-- closing "container_16" -->
 </body>
-<!--<div  style="border: 1px solid #fff;height: 200px; width: 130px; float:left;"></div>
-<div  style="border: 1px solid #fff;height: 200px; width: 800px; float:right;"></div>
---!>
--- layouts/header.php
+++ layouts/header.php
@@ -1,34 +1,34 @@
 <div class="grid_16">
-  <div id="header" >
-    <div class="grid_5 alpha">
-      <a href="www.frontlinecitychurch.com">
-        <img src="images/logo.png"></img>
+  <div id="header">
+    <div class="grid_3 alpha logo">
+      <a href="http://www.frontlinecitychurch.com">
+        <img src="/images/logo.png"></img>
       </a>
     </div>
     <li class="grid_1">&nbsp</li>
     <ul id="nav" class="sf-menu">
       <li class="grid_2"><a href="#">New Here?</a>
       <ul>
-        <li><a href="#">Welcome</a></li>
-        <li><a href="#">The Gospel</a></li>
-        <li><a href="#">Who We Are</a></li>
-        <li><a href="#">Why We Exist</a></li>
-        <li><a href="#">What We Believe</a></li>
-        <li><a href="#">Fronline Ministries</a></li>
+        <li><a href="/new_here/welcome.php">Welcome</a></li>
+        <li><a href="/new_here/gospel.php">The Gospel</a></li>
+        <li><a href="/new_here/who.php">Who We Are</a></li>
+        <li><a href="/new_here/why.php">Why We Exist</a></li>
+        <li><a href="/new_here/what.php">What We Believe</a></li>
+        <li><a href="/new_here/frontline.php">Fronline Ministries</a></li>
       </ul>
       </li>
       <li class="grid_3"><a href="#">When & Where</a>
       <ul>
-        <li><a href="#">Community Groups</a></li>
-        <li><a href="#">Weekly Gatherings</a></li>
-        <li><a href="#">Monthly Gatherings</a></li>
-        <li><a href="#">Calendar</a></li>
+        <li><a href="/when_and_where/community.php">Community Groups</a></li>
+        <li><a href="/when_and_where/weekly.php">Weekly Gatherings</a></li>
+        <li><a href="/when_and_where/monthly.php">Monthly Gatherings</a></li>
+        <li><a href="/when_and_where/calendar.php">Calendar</a></li>
       </ul>
       </li>
       <li class="grid_2"><a href="#">Media</a>
       <ul>
-        <li><a href="#">Podcasts</a></li>
-        <li><a href="#">Videos</a></li>
+        <li><a href="http://www.vimeo.com/user6107401/videos">Podcasts</a></li>
+        <li><a href="http://itunes.apple.com/us/podcast/frontline-city-church/id416154894?ign-mpt=uo%3D4">Videos</a></li>
       </ul>
       </li>
       <li class="grid_2"><a href="#">Give</a></li>
