da1fde8ba5e1e2846dc904b0fa34b1d7410b2fb2
Tyler Copple <J.TylerCopple@gmail.com>
2011-04-07 21:57:14 -0400

    fixed some general page layout issues and added content for monthly.php

--- when_and_where/monthly.php
+++ when_and_where/monthly.php
@@ -1,25 +1,90 @@
 <?php 
-  include_once("../utilities.php"); 
-  start(); 
+include_once("../utilities.php"); 
+start(); 
 ?>
+
+<!-- this page is absolutely awful but the information given was not very workable
+     and it shouldn't be up more than a few months, that's why everything is inlined -->
+
 <div id="page_content" class="shadow">
   <div id="text" >
     <h3 class="yellow bold">Monthly</h3>
     <p>
-      Over the course of the next 5 months, we will be holding Monthly Gatherings at different locations all over New Haven.  Please join.
+    Over the course of the next 5 months, we will be holding Monthly Gatherings at different locations all over New Haven.  Please join.
     </p>
-    <div>
-      <img src="/images/fcc_logo_toadsplace.png"></img>
+    <div class="clear" style="height: 15px;"></div>
+    <div class="block" style="height: 170px; padding: 15px 0px; width: 100%;">
+      <div class="inline left">
+        <img src="/images/fcc_logo_toadsplace.png"></img>
+      </div>
+      <div class="inline middle" >
+        April 24th, 2001 - 10am<br>
+        Location: Toad's Place<br>
+        300 York St.<br>
+        New Haven, CT 06511<br>
+      </div>
     </div>
-    <div>
-      <img src="/images/fcc_logo_thestudy.png"></img>
+
+    <div class="clear" style="height: 7px;"></div>
+    <div style="height: 1px; border: 1px solid #ccc;"></div>
+    <div class="clear" style="height: 7px;"></div>
+
+    <div class="block" style=" padding: 15px 0px; width: 100%;">
+      <div class="inline left">
+        <img src="/images/fcc_logo_thestudy.png"></img>
+      </div>
+      <div class="inline ">
+        May 29th, 2011 - 6pm<br>
+        Location: The Study At Yale<br>
+        1157 Chapel St.<br>
+        New Haven, CT 06511<br> 
+      </div>
     </div>
-    <div>
-      <img src="/images/fcc_map_thestudy.png"></img>
+
+    <div class="clear" style="height: 7px;"></div>
+    <div style="height: 1px; border: 1px solid #ccc;"></div>
+    <div class="clear" style="height: 7px;"></div>
+
+    <div class="block" style="height: 180; padding: 15px 0px; width: 100%;">
+      <div class="inline left">
+        <img src="/images/fcc_map_thestudy.png"></img>
+      </div>
+      <div class="inline">
+        June 26th, 2011 - 6pm<br>
+        Location: The Study At Yale<br>
+        1157 Chapel St.<br>
+        New Haven, CT 06511<br>
+      </div>
     </div>
-    <div>
-      <img src="/images/fcc_image_coop.png"></img>
+
+    <div class="clear" style="height: 7px;"></div>
+    <div style="height: 1px; border: 1px solid #ccc;"></div>
+    <div class="clear" style="height: 7px;"></div>
+    
+    <div class="block" style="padding: 15px 0px; width: 100%;">
+      <center>
+      July 24th, 2011 - 12pm<br>
+      Location: The New Haven Green<br>
+      Food and Fun for all!<br>
+    </center>
     </div>
+
+    <div class="clear" style="height: 7px;"></div>
+    <div style="height: 1px; border: 1px solid #ccc;"></div>
+    <div class="clear" style="height: 7px;"></div>
+    
+    <div class="block"  style="height: 230; padding: 15px 0px; width: 100%;">
+      <div class="inline left">
+        <img src="/images/fcc_image_coop.png"></img>
+      </div>
+      <div class="inline">
+        August 21st, 2011 - 6pm<br>
+        Location: New Haven Co-op High School<br>
+        177 College St.<br> 
+        New Haven, CT 06511<br>
+      </div>
+    </div>
+    <div class="clear" style="height: 20px;"></div>
   </div>
 </div>
 <?php stop(); ?>
