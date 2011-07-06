873acde56f293cdf07d73dd17d913d4dad105753
Tyler Copple <J.TylerCopple@gmail.com>
2011-07-06 00:42:00 -0400

    updating i heart new haven day information

--- when_and_where/monthly.php
+++ when_and_where/monthly.php
@@ -13,60 +13,17 @@ start();
     Over the course of the next 5 months, we will be holding Monthly Gatherings at different locations all over New Haven.  Please join.
     </p>
     <div class="clear" style="height: 15px;"></div>
-    <div class="block" style="height: 170px; padding: 15px 0px; width: 100%;">
-      <div class="inline left">
-        <img src="/images/fcc_logo_toadsplace.png"></img>
-      </div>
-      <div class="inline middle" >
-        April 24th, 2001 - 10am<br>
-        Location: Toad's Place<br>
-        300 York St.<br>
-        New Haven, CT 06511<br>
-      </div>
-    </div>
-
-    <div class="clear" style="height: 7px;"></div>
-    <div style="height: 1px; border: 1px solid #ccc;"></div>
-    <div class="clear" style="height: 7px;"></div>
-
-    <div class="block" style=" padding: 15px 0px; width: 100%;">
-      <div class="inline left">
-        <img src="/images/fcc_logo_thestudy.png"></img>
-      </div>
-      <div class="inline ">
-        May 29th, 2011 - 6pm<br>
-        Location: The Study At Yale<br>
-        1157 Chapel St.<br>
-        New Haven, CT 06511<br> 
-      </div>
-    </div>
-
-    <div class="clear" style="height: 7px;"></div>
-    <div style="height: 1px; border: 1px solid #ccc;"></div>
-    <div class="clear" style="height: 7px;"></div>
-
-    <div class="block" style="height: 180; padding: 15px 0px; width: 100%;">
-      <div class="inline left">
-        <img src="/images/fcc_map_thestudy.png"></img>
-      </div>
-      <div class="inline">
-        June 26th, 2011 - 6pm<br>
-        Location: The Study At Yale<br>
-        1157 Chapel St.<br>
-        New Haven, CT 06511<br>
-      </div>
-    </div>
-
-    <div class="clear" style="height: 7px;"></div>
-    <div style="height: 1px; border: 1px solid #ccc;"></div>
     <div class="clear" style="height: 7px;"></div>
     
     <div class="block" style="padding: 15px 0px; width: 100%;">
-      <center>
-      July 24th, 2011 - 12pm<br>
-      Location: The New Haven Green<br>
-      Food and Fun for all!<br>
-    </center>
+      <div class="inline left">
+        <img src="/images/i_heart_logo.png"></img>
+      </div>
+      <div class="inline">
+        July 24th, 2011 - 3pm<br>
+        Location: The New Haven Green<br>
+        Food and Fun for all!<br>
+      </div>
     </div>
 
     <div class="clear" style="height: 7px;"></div>
