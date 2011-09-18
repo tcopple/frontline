684da6832e10e3c8e990742c62b20403bfd58337
Tyler Copple <J.TylerCopple@gmail.com>
2011-09-17 22:20:07 -0400

    fixing slideshow bug, after removing a slide

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,5 @@
 var slide = 1;
-var slides = 3;
+var slides = 2;
 var slideshow_interval_id = null;
 
 function signal_slideshow() {
@@ -12,16 +12,16 @@ function signal_slideshow() {
 }
 
 $(document).ready( function() {
-  
+
   $('ul.slideshow li:gt(0)').hide();
-  signal_slideshow(); 
+  signal_slideshow();
 });
 
 function slideshow() {
   $('div#main li:nth-child(' + slide + ')').fadeOut('slow', function() {
-  
+
     slide = slide + 1;
-    if (slide > slides)  
+    if (slide > slides)
       slide = 1;
 
     $('div#main li:nth-child(' + slide + ')').fadeIn('slow');
