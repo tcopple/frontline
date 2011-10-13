ca74b612fecac22c55b2cfdaa41c87539d89720b
Tyler Copple <J.TylerCopple@gmail.com>
2011-10-13 00:38:57 -0400

    calendar page updated with google js

--- when_and_where/calendar.php
+++ when_and_where/calendar.php
@@ -1,11 +1,16 @@
-<?php 
-  include_once("../utilities.php"); 
-  start(); 
+<?php
+  include_once("../utilities.php");
+  start(
+     "<script type=\"text/javascript\" src=\"http://www.google.com/jsapi\"></script>
+      <script type=\"text/javascript\" src=\"../js/google_calendar.js\"></script>"
+  );
 ?>
+
 <div id="page_content" class=" shadow">
   <div id="text" >
     <h3 class="yellow bold">Calendar</h3>
-      Coming soon... 
+    <div id="events">
+    </div>
   </div>
 </div>
 <?php stop(); ?>
